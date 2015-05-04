060b35db340956cc1156d58c1a214dad6e90cc19
Marc Bluemner <marc.bluemner@gmail.com>
2015-05-04 17:28:39 +0200

    version namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # API definition
   namespace :api, defaults: { format: :josn },
               constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1 do
 
+    end
   end
 end
