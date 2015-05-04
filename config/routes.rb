84c45439f20983945a488afcd18097d7a946a221
Marc Bluemner <marc.bluemner@gmail.com>
2015-05-04 18:47:49 +0200

    Add devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
+require "api_constraints"
+
 Rails.application.routes.draw do
+  devise_for :users
   # API definition
   namespace :api, defaults: { format: :josn },
               constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do
 
-    end
+      end
   end
 end
